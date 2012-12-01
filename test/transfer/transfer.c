27be2b12ac6a356b4e2922c18c45fbfc994cf443
Arjit Chattopadhyay <Arjit Chattopadhyay>
2012-12-01 11:57:52 -0500

    Testing tool modified

--- test/transfer/transfer.c
+++ test/transfer/transfer.c
@@ -10,7 +10,7 @@ int main(){
 	struct 	timespec starttime;
 	struct timespec endtime;
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&starttime);
-	Hyb_ASend(0,data,count);
+	Hyb_Send(0,data,count);
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&endtime);
 	printf("Time taken in nano secs %lu\n",(endtime.tv_nsec-starttime.tv_nsec));
 	return 0;
